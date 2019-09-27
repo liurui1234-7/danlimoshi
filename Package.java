78760caa6d3a00eabac7be69d44fec815a9c3eac
unknown <you@example.com>
2019-09-27 15:26:03 +0800

    新增一个Package文件

---
+++ Package.java
@@ -0,0 +1,22 @@
+ class Course{
+         public Course(){
+         System.out.println("this is a course");
+}
+}
+class Student{
+         public Student(){
+         System.out.println("this is a student");
+}
+}
+ class Teacher{
+         public Teacher(){
+         System.out.println("this is a course");
+}
+}
+public class Package{
+public static void main(String[] args){
+	Student a = new Student();
+	Teacher te = new Teacher();
+	Course cs = new Course();	
+}	
+}
