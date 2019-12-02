29b2735d1150ce9e6cc5b04fdbf53ef0e6eb4f68
unknown <you@example.com>
2019-12-02 20:26:23 +0800

    PBL第二版本之搬移金额代码之把accountFor()移到Rental中

---
+++ 搬移金额代码之把accountFor()移到Rental中/src/rental/Rental1.java
@@ -0,0 +1,38 @@
+package rental;
+import move.*;
+
+public class Rental1 {
+	private Movie _movie;
+	private int _daysRented;
+	//将电影名和每日租金收入存入
+	public Rental1(Movie movie,int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+	public int getDaysRented() {
+		// TODO Auto-generated method stub
+		return _daysRented;
+	}
+	public Movie getMovie() {
+		return _movie;
+	}
+	public double getCharge() {
+		double reasult=0;
+		   switch(getMovie().getPriceCode()) {
+		 case Movie.REGULAR:
+			 reasult+=2;
+			 if(getDaysRented()>2)
+				 reasult +=(getDaysRented() -2)*1.5;
+			 break;
+		 case Movie.NEW_RELEASE:
+			 reasult += getDaysRented()*3;
+			 break;
+		 case Movie.CHILDRENS :
+			 reasult += 1.5;
+			 if(getDaysRented()>3)
+				 reasult +=(getDaysRented() - 3)*1.5;
+			 break;
+	}
+		   return reasult;
+	}
+}
